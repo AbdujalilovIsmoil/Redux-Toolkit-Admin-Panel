fa1b3e1264e1688b56b3c823021450980c2ebf8f
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-07-27 22:21:07 +0300

    refactor: ts folder changed

--- src/ts/interfaces/index.ts
+++ src/ts/interfaces/index.ts
@@ -1,5 +1,4 @@
 import { inputInterface } from "./input.interfaces";
 import { buttonInterface } from "./button.interfaces";
-import { textereaInterface } from "./texterea.interfaces";
 
-export type { inputInterface, buttonInterface, textereaInterface };
+export type { inputInterface, buttonInterface };
--- src/ts/interfaces/texterea.interfaces.ts
+++
@@ -1,4 +0,0 @@
-export interface textereaInterface {
-  type?: string;
-  children?: string;
-}
