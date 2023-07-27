20b2a292cfd30af71e62283654adec6f1f094aeb
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-07-27 19:50:17 +0300

    feat: redux-toolkit and react-redux state managements added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import App from "./App.tsx";
+import { store } from "./store";
 import "./assets/styles/index.scss";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,7 +11,9 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <QueryClientProvider client={client}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </>
