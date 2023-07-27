2922c0a01977621890bccc6d3e1cf4d35b18f216
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-07-27 19:23:20 +0300

    feat: react-router-dom library added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import {} from "react";
+import Routes from "./routes";
+import { Home, Error } from "./pages";
+import { lazy, Suspense } from "react";
+import { Loader } from "./components/layouts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = (): JSX.Element => {
+  const Login = lazy(() => import("./pages/Login"));
+  const Registration = lazy(() => import("./pages/Registration"));
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Routes />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <Error />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/registration",
+      element: <Registration />,
+    },
+  ]);
+
   return (
     <>
-      <h1>App</h1>
+      <Suspense fallback={<Loader />}>
+        <RouterProvider router={routes} />
+      </Suspense>
     </>
   );
 };
